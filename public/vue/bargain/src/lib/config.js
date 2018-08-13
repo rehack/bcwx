ce01a0e1cbfd0aa46cdf9c097442d718b53cc5db
codesboy <565195693@qq.com>
2018-08-13 23:01:17 +0800

    update tp5.1.22

--- public/vue/bargain/src/lib/config.js
+++ public/vue/bargain/src/lib/config.js
@@ -1,14 +1,10 @@
-// 后台接口主域名
-// const APIHOST ='https://bcwx.rehack.cn'
-// const APIHOST ='http://192.168.1.253'
-
 const MYCONF = {
     // 后台接口主域名
-    api_host:'https://bcwx.rehack.cn',
-    // api_host:'http://192.168.1.253',
+    // api_host:'https://bcwx.rehack.cn',
+    api_host:'http://192.168.1.253',
     // 测试号appid
-    // appid:'wx150347fed55855dd'
-    appid:'wxc373be0ebd319f82'
+    appid:'wx150347fed55855dd'
+    // appid:'wxc373be0ebd319f82'
 }
 
 export default MYCONF
