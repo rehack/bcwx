d8ce45ccabffedb0f1cb685791179efa1298efef
Rehack <rehack@RehackdeiMac.local>
2019-04-12 14:39:25 +0800

    update .gitignore

--- application/wx_bargain_api/config/wechat.php
+++
@@ -1,7 +0,0 @@
-<?php
-// 微信配置文件
-return [
-    'app_id'=>'wx150347fed55855dd',
-    'app_secret'=>'29ecd0afcf060a72a0c63a9ce275f3b4',
-    
-];
--- public/vue/bargain/vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    //部署路径
-    baseUrl:'/bargain/',
-    // 打包文件放置路径
-    outputDir:'../../bargain',
-    /* devServer:{
-        port:80
-    } */
-}
